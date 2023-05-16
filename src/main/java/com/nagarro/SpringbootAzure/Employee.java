d8028cf533664ea6a952c3ebd3c4b91964f4d432
nazim khan <nazim.khan@nagarro.com>
2023-05-16 20:21:00 +0530

    code

---
+++ src/main/java/com/nagarro/SpringbootAzure/Employee.java
@@ -0,0 +1,23 @@
+package com.nagarro.SpringbootAzure;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String department;
+    private long salary;
+
+
+}
